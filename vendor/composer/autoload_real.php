623a1e6f643352428b76e91246ef139b80ed1f81
IMHOTEP <boua.mbo@uvci.edu.ci>
2020-11-13 15:04:58 +0000

    Update Guzzle

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit446f492f3c1135aa9e53103bf989f7f8
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit446f492f3c1135aa9e53103bf989f7f8', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit446f492f3c1135aa9e53103bf989f7f8', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit446f492f3c1135aa9e53103bf989f7f8::getInitializer($loader));
         } else {
